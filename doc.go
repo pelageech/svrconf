4710bd58fb8140b32c13ff73bf6ae7583c5605d3
pelageech <va.oleg2015@yandex.ru>
2023-11-22 21:34:32 +0700

    new comment

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 // Package svrconf provides the structure of http.Server
-// configuration and a loader for it.
+// configuration and a loader for its fields.
 //
 // The package supports config loader for YAML-files.
 package svrconf
